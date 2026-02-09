f5a9a97f5365b4b4271e4ea46ac30c0eda8ae467
Will Reisig <williamreisig@u.boisestate.edu>
2026-02-09 11:08:09 -0700

    Finished pingsweep.sh

--- pingsweep/pingsweep.sh
+++ pingsweep/pingsweep.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
-# Ping Sweep Script - IP range or hostname prefix
 
-echo "TODO: Implement the ping sweep script according to the specifications in the README.md file."
+base="onyxnode"
+
+usage () {
+echo "Usage: ./pingsweep.sh <-h|-p>"
+}
+
+ping_sweeper () {
+> ping.log
+local total=0
+local found=0
+for i in {1..200}
+do
+        echo "$base$i"
+        ping -c 1 -w 1 $base$i >> temp.log
+	if [ $? -eq 0 ]; then
+		echo "${base}${i} found." >> ping.log
+		found=$((found+1))
+	else
+		echo "Node ${i} not found."
+	fi
+	total=$((total +1))
+done
+echo "Total nodes found: ${found} / ${total}" >> ping.log
+rm -f temp.log
+}
+
+main () {
+if [[ $# > 1 ]]; then
+	usage
+elif [[ $1 == "-h" ]]; then
+	usage
+elif [[ $1 == "-p" ]]; then
+	echo = "Sweeping..."
+	ping_sweeper
+else
+	usage
+fi
+}
+
+main "$@"
